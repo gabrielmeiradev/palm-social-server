91dce8aa62dcf1a09cfaa3939d784e050f5bb3bf
gabrielmeiradev <gabriel_meira10@hotmail.com>
2025-02-26 12:27:32 -0300

    fix: now checking if request have page param less than 0

--- src/controllers/post/get-all.ts
+++ src/controllers/post/get-all.ts
@@ -5,72 +5,85 @@ import { StatusCodes } from "http-status-codes";
 const prisma = new PrismaClient();
 
 export const getAllPosts = async (req: Request, res: Response) => {
-  const { text, hashtags, page = 1, itemsPerPage = 10 } = req.query;
+  try {
+    const { text, hashtags, page = 1, itemsPerPage = 10 } = req.query;
 
-  const hashtagsArray = hashtags ? (hashtags as string).split(",") : [];
+    const hashtagsArray = hashtags ? (hashtags as string).split(",") : [];
 
-  const pageNumber = parseInt(page as string, 10);
-  const itemsCount = parseInt(itemsPerPage as string, 10);
+    const pageNumber = parseInt(page as string, 10);
+    const itemsCount = parseInt(itemsPerPage as string, 10);
 
-  const [posts, totalPosts] = await Promise.all([
-    prisma.post.findMany({
-      where: {
-        text_content: {
-          contains: text as string,
-          mode: "insensitive",
-        },
-        AND:
-          hashtagsArray.length > 0
-            ? {
-                hashtags: {
-                  some: {
-                    title: {
-                      in: hashtagsArray,
-                    },
-                  },
-                },
-              }
-            : {},
-      },
-      include: {
-        hashtags: {
-          select: {
-            title: true,
+    if (pageNumber < 1) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: "O número da página deve ser maior que 0.",
+      });
+    }
+
+    const [posts, totalPosts] = await Promise.all([
+      prisma.post.findMany({
+        where: {
+          text_content: {
+            contains: text as string,
+            mode: "insensitive",
           },
-        },
-        author: true,
-      },
-      skip: (pageNumber - 1) * itemsCount,
-      take: itemsCount,
-    }),
-    prisma.post.count({
-      where: {
-        text_content: {
-          contains: text as string,
-        },
-        AND:
-          hashtagsArray.length > 0
-            ? {
-                hashtags: {
-                  some: {
-                    title: {
-                      in: hashtagsArray,
+          AND:
+            hashtagsArray.length > 0
+              ? {
+                  hashtags: {
+                    some: {
+                      title: {
+                        in: hashtagsArray,
+                      },
                     },
                   },
-                },
-              }
-            : {},
-      },
-    }),
-  ]);
+                }
+              : {},
+        },
+        include: {
+          hashtags: {
+            select: {
+              title: true,
+            },
+          },
+          author: true,
+        },
+        skip: (pageNumber - 1) * itemsCount,
+        take: itemsCount,
+      }),
+      prisma.post.count({
+        where: {
+          text_content: {
+            contains: text as string,
+          },
+          AND:
+            hashtagsArray.length > 0
+              ? {
+                  hashtags: {
+                    some: {
+                      title: {
+                        in: hashtagsArray,
+                      },
+                    },
+                  },
+                }
+              : {},
+        },
+      }),
+    ]);
 
-  res.status(StatusCodes.OK).json({
-    posts,
-    pagination: {
-      totalItems: totalPosts,
-      currentPage: pageNumber,
-      itemsPerPage: itemsCount,
-      totalPages: Math.ceil(totalPosts / itemsCount),
-    },
-  });
+    res.status(StatusCodes.OK).json({
+      posts,
+      pagination: {
+        totalItems: totalPosts,
+        currentPage: pageNumber,
+        itemsPerPage: itemsCount,
+        totalPages: Math.ceil(totalPosts / itemsCount),
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Ocorreu um erro ao buscar as postagens.",
+    });
+  }
 };
