f8da39bc75f6f95fa352d437061211963cad8168
gabrielmeiradev <gabriel_meira10@hotmail.com>
2025-03-07 09:54:32 -0300

    feat: get all hashtags

--- src/controllers/hashtag/get-all.ts
+++ src/controllers/hashtag/get-all.ts
@@ -1,19 +1,36 @@
 import { Request, Response } from "express";
-
 import { PrismaClient } from "@prisma/client";
 import { StatusCodes } from "http-status-codes";
 
 const prisma = new PrismaClient();
 
-export const getAllHashtags = async (_: Request, res: Response) => {
+export const getAllHashtags = async (req: Request, res: Response) => {
+  const { page = 1, limit = 10 } = req.query;
+
+  const pageNumber = parseInt(page as string, 10);
+  const limitNumber = parseInt(limit as string, 10);
+
   try {
     const hashtags = await prisma.hashtag.findMany({
+      skip: (pageNumber - 1) * limitNumber,
+      take: limitNumber,
       select: {
         title: true,
       },
     });
 
-    res.status(StatusCodes.OK).json(hashtags);
+    const totalHashtags = await prisma.hashtag.count();
+    const totalPages = Math.ceil(totalHashtags / limitNumber);
+
+    res.status(StatusCodes.OK).json({
+      hashtags,
+      pagination: {
+        totalHashtags,
+        totalPages,
+        currentPage: pageNumber,
+        limit: limitNumber,
+      },
+    });
   } catch (error) {
     console.error(error);
     res
