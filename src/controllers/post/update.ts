8812355595da91eca8ea915524e3f9bc9b74f35a
gabrielmeiradev <gabriel_meira10@hotmail.com>
2025-03-11 15:47:03 -0300

    fix: update hashtags logic

--- src/controllers/post/update.ts
+++ src/controllers/post/update.ts
@@ -7,7 +7,15 @@ export const updatePost = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { text_content, hashtags } = req.body;
 
-  const hashtagsArray = hashtags.split(",");
+  let hashtagsArray = hashtags?.split(",") ?? [];
+
+  const extractedHashtags = text_content.match(/#[a-zA-Z0-9_]+/g) || [];
+  hashtagsArray = [
+    ...new Set([
+      ...hashtagsArray,
+      ...extractedHashtags.map((ht: string) => ht.slice(1)),
+    ]),
+  ];
 
   try {
     const post = await prisma.post.update({
